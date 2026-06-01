8bbbd95bb058d692894814ccded2d6b7b657bf8c
unknown <caleb.ayew-ew@configura.com>
2026-06-01 15:35:44 -0400

    removed build files

--- .runtime/tk/tk8.6/pkgIndex.tcl
+++ .runtime/tk/tk8.6/pkgIndex.tcl
@@ -1,7 +1,7 @@
 if {![package vsatisfies [package provide Tcl] 8.6.0]} return
 if {($::tcl_platform(platform) eq "unix") && ([info exists ::env(DISPLAY)]
 	|| ([info exists ::argv] && ("-display" in $::argv)))} {
-    package ifneeded Tk 8.6.12 [list load "C:/Users/yomeg/AppData/Local/Programs/Python/Python311/DLLs/tk86t.dll"]
+    package ifneeded Tk 8.6.14 [list load "C:/Users/calay/AppData/Local/Programs/Python/Python313/DLLs/tk86t.dll"]
 } else {
-    package ifneeded Tk 8.6.12 [list load "C:/Users/yomeg/AppData/Local/Programs/Python/Python311/DLLs/tk86t.dll"]
+    package ifneeded Tk 8.6.14 [list load "C:/Users/calay/AppData/Local/Programs/Python/Python313/DLLs/tk86t.dll"]
 }
